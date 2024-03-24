30be8b78efae26f384ca161d6b3febcf63b6d6a1
eeryyy282 <airiagustus82@gmail.com>
2024-03-25 06:09:20 +0700

    adjust gradle version and style

--- app/src/main/java/com/example/noteappswithroom/database/NoteDao.kt
+++ app/src/main/java/com/example/noteappswithroom/database/NoteDao.kt
@@ -1,12 +1,15 @@
 package com.example.noteappswithroom.database
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 
+
+@Dao
 interface NoteDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(note: Note)
